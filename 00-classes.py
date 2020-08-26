2eff9d5abd8ca29721a9aa30d5a217b4f43881dd
Vasily <post_lv@mail.ru>
2020-08-26 11:39:42 +0300

    init

---
+++ 00-classes.py
@@ -0,0 +1,25 @@
+class Human:
+    first_name: str
+    last_name: str
+
+    def __init__(self, first_name, last_name="Undefind"):
+        self.first_name = first_name
+        self.last_name = last_name
+    
+    def __str__(self):
+        return f'User: {self.first_name} {self.last_name}'
+
+
+class User(Human):
+    __passport: str = "124334353 3434"
+    
+    def hello(self):
+        print(f'hello, {self.first_name} {self.last_name}')
+
+john = User("John", "Doe")
+#john.hello()
+
+artur = User("Artur")
+#artur.hello()
+
+print(john)
